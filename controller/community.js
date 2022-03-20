a63d503653f62255c8a9fd9ff30795bf2b4b4c2c
“Oreolv” <455001000@qq.com>
2022-03-21 00:45:04 +0800

    feat: 新增社区接口

--- controller/community.js
+++ controller/community.js
@@ -6,3 +6,29 @@ const getCommunityList = async () => {
   const ret = await community.findAll();
   return new SuccessModel('获取成功', ret);
 };
+
+const createNewCommunity = async (name, custodian, description) => {
+  const { username } = await users.findOne({
+    where: {
+      id: custodian,
+    },
+  });
+  await community.create({
+    name,
+    custodian: username,
+    description,
+    createTime: new Date(),
+  });
+  await users.update(
+    { cname: name },
+    {
+      where: {
+        id: custodian,
+      },
+    }
+  );
+  // TODO 错误处理
+  return new SuccessModel('创建成功');
+};
+
+module.exports = { getCommunityList, createNewCommunity };
--- routes/community.js
+++ routes/community.js
@@ -1,9 +1,18 @@
 const router = require('koa-router')();
-const { getCommunityList } = require('../controller/community');
+const {
+  getCommunityList,
+  createNewCommunity,
+} = require('../controller/community');
 router.prefix('/community');
 
 router.get('/getCommunityList', async function (ctx, next) {
   const result = await getCommunityList();
+  ctx.body = result;
+});
+
+router.post('/createNewCommunity', async function (ctx, next) {
+  const { name, custodian, description } = ctx.request.body;
+  const result = await createNewCommunity(name, custodian, description);
   ctx.body = result;
 });
 
