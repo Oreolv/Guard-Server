293db251c5c8225b7995a1e51d5073ddc8d1185c
“Oreolv” <455001000@qq.com>
2022-03-21 20:44:06 +0800

    feat: 修改token中间件返回错误

--- middlewares/tokenHandler.js
+++ middlewares/tokenHandler.js
@@ -1,8 +1,8 @@
-const { ErrorModel } = require('../model/response');
-const jwt = require('jsonwebtoken');
 const util = require('util');
+const jwt = require('jsonwebtoken');
 const verify = util.promisify(jwt.verify);
 const { jwtSecret } = require('../config/secret');
+const { ErrorModel } = require('../model/response');
 
 module.exports = function () {
   return async function (ctx, next) {
@@ -15,16 +15,13 @@ module.exports = function () {
           roleValue: payload.roleValue,
         };
       }
-      await next();
     } catch (err) {
       if (err.status === 401) {
         ctx.status = 401;
-        ctx.body = new ErrorModel('登陆认证失败，缺少token信息', 401);
-      } else {
-        ctx.status = 500;
-        ctx.body = new ErrorModel('未知错误');
-        console.log(err);
+        ctx.body = new ErrorModel('登陆认证失败, 缺少token信息', 401);
       }
+    } finally {
+      await next();
     }
   };
 };
