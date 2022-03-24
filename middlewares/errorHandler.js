0cd7d889a9b71b5b44e14398162a068271b252b7
“Oreolv” <455001000@qq.com>
2022-03-24 23:27:37 +0800

    feat: 增加错误返回信息

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,13 +5,19 @@ module.exports = function () {
     try {
       await next();
     } catch (err) {
-      console.log(err);
+      console.log('error-----', err);
+      console.log('name-----', err.name);
+      console.log('message-----', err.message);
+      console.log('status-----', err.status);
       switch (err.name) {
         case 'SequelizeUniqueConstraintError':
           ctx.body = new ErrorModel(`该${ctx.errorKey}已存在`);
           break;
         case 'SequelizeDatabaseError':
           ctx.body = new ErrorModel('数据库查询失败');
+          break;
+        case 'UnauthorizedError':
+          ctx.body = new ErrorModel('请先登陆');
           break;
         default:
           ctx.body = new ErrorModel(err.message);
