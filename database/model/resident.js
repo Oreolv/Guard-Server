fd3e0e0ddf970a623d2b030020cd4d8addf8d649
“Oreolv” <455001000@qq.com>
2022-03-25 13:27:19 +0800

    fix: 修复手机号长度问题

--- database/model/resident.js
+++ database/model/resident.js
@@ -18,7 +18,7 @@ const resident = sequelize.define(
     // 个人信息区
     uname: Sequelize.STRING(10), // 姓名
     usex: Sequelize.INTEGER(1), // 性别
-    uphone: Sequelize.INTEGER(11), // 手机号
+    uphone: Sequelize.STRING(11), // 手机号
     cname: Sequelize.STRING(20), // 社区名称
     vname: Sequelize.STRING(20), // 小区名称
     bnum: Sequelize.STRING(5), // 楼栋号
@@ -30,7 +30,7 @@ const resident = sequelize.define(
     // 外地旅居信息
     trip: Sequelize.STRING(20), // 14天去往其他省市 无/市（以英文逗号分割）
     riskStatus: Sequelize.INTEGER(1), // 14天去往其他省市是否是中高风险地区 0否1是
-    vehicle: Sequelize.INTEGER(1), // 乘坐交通工具 0驾车1火车2高铁3飞机
+    vehicle: Sequelize.INTEGER(1), // 乘坐交通工具 0驾车1大巴2火车3高铁4飞机
     vehicleNo: Sequelize.STRING(20), // 乘坐车牌号/车次/航班号
     vehicleSeat: Sequelize.STRING(20), // 乘坐座位号，自驾则填无
     // 隔离信息
@@ -39,7 +39,7 @@ const resident = sequelize.define(
     accessStatus: Sequelize.INTEGER(1), // 限制出入：0否1是
     isolationStart: Sequelize.DATE(), // 隔离开始时间
     isolationEnd: Sequelize.DATE(), // 隔离结束时间
-    administrator: Sequelize.STRING(20), // 关联users.id, 显示 users.name
+    administrator: Sequelize.STRING(20), // 负责人 关联users.id, 显示 users.name
     // 填报信息
     recorder: Sequelize.STRING(20), // 关联users.id, 显示 users.name
     createTime: Sequelize.DATE(), // 填报日期
