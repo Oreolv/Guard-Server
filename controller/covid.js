6ae884c302cbcdce2a6ba1abaf331116db54f57d
“Oreolv” <455001000@qq.com>
2022-03-31 15:54:40 +0800

    feat: 获取疫情风险地区

--- controller/covid.js
+++ controller/covid.js
@@ -4,14 +4,32 @@ const { SuccessModel, ErrorModel } = require('../model/response');
 
 const getAllData = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, '../static/all_data.json'), function (err, data) {
-      if (err) {
-        reject(new ErrorModel('获取失败'));
+    fs.readFile(
+      path.join(__dirname, '../static/all_data.json'),
+      function (err, data) {
+        if (err) {
+          reject(new ErrorModel('获取失败'));
+        }
+        const json = JSON.parse(data.toString());
+        resolve(new SuccessModel('获取成功', json));
       }
-      const json = JSON.parse(data.toString());
-      resolve(new SuccessModel('获取成功', json));
-    });
+    );
   });
 };
 
-module.exports = { getAllData };
+const getRiskArea = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, '../static/risk_area.json'),
+      function (err, data) {
+        if (err) {
+          reject(new ErrorModel('获取失败'));
+        }
+        const json = JSON.parse(data.toString());
+        resolve(new SuccessModel('获取成功', json));
+      }
+    );
+  });
+};
+
+module.exports = { getAllData, getRiskArea };
--- routes/covid.js
+++ routes/covid.js
@@ -1,9 +1,14 @@
 const router = require('koa-router')();
-const { getAllData } = require('../controller/covid');
+const { getAllData, getRiskArea } = require('../controller/covid');
 router.prefix('/covid');
 
 router.get('/all_data', async function (ctx, next) {
   const result = await getAllData();
+  ctx.body = result;
+});
+
+router.get('/risk_area', async function (ctx, next) {
+  const result = await getRiskArea();
   ctx.body = result;
 });
 
