11061610d9fa8a3ea0f1419f0083dbc87318ad25
“Oreolv” <455001000@qq.com>
2022-04-01 10:20:02 +0800

    feat: 精简读取json函数

--- controller/covid.js
+++ controller/covid.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const { SuccessModel, ErrorModel } = require('../model/response');
 
-const getAllData = () => {
+const readFile = dir => {
   return new Promise((resolve, reject) => {
     fs.readFile(
-      path.join(__dirname, '../static/all_data.json'),
+      path.join(__dirname, `../static/${dir}.json`),
       function (err, data) {
         if (err) {
           reject(new ErrorModel('获取失败'));
@@ -17,19 +17,12 @@ const getAllData = () => {
   });
 };
 
+const getAllData = () => {
+  return readFile('all_data');
+};
+
 const getRiskArea = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(
-      path.join(__dirname, '../static/risk_area.json'),
-      function (err, data) {
-        if (err) {
-          reject(new ErrorModel('获取失败'));
-        }
-        const json = JSON.parse(data.toString());
-        resolve(new SuccessModel('获取成功', json));
-      }
-    );
-  });
+  return readFile('risk_area');
 };
 
 module.exports = { getAllData, getRiskArea };
