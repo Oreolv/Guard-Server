b70b5c14f2bb3886af7f193c62c10bfa5ea22c78
“Oreolv” <455001000@qq.com>
2022-04-09 11:52:10 +0800

    feat: 适配根据用户查询社区

--- controller/community.js
+++ controller/community.js
@@ -3,22 +3,27 @@ const Users = require('../database/model/users');
 const Community = require('../database/model/Community');
 const { SuccessModel } = require('../model/response');
 
-const getCommunityList = async (name = '') => {
-  const whereObject = {};
-  if (name) {
-    whereObject.name = { [Op.like]: `%${name}%` };
-  }
-
+const getCommunityList = async (name = '', custodian = '') => {
+  const nameWhereObject = name ? { name: { [Op.like]: `%${name}%` } } : null;
+  // const custodianWhereObject = custodian ? { id: custodian } : null;
+  // TODO: 在include里面使用where会导致users里面只出现查找的那个user
   const ret = await Community.findAll({
     include: [
       {
         model: Users,
         attributes: ['id', 'username', 'realName'],
         through: { attributes: [] },
+        // where: custodianWhereObject,
       },
     ],
-    where: whereObject,
+    where: nameWhereObject,
   });
+  if (custodian) {
+    return new SuccessModel(
+      '获取成功',
+      ret.filter(i => JSON.stringify(i.users).includes(`"id":${custodian}`))
+    );
+  }
   return new SuccessModel('获取成功', ret);
 };
 
