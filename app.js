fe923a04259946bdfd43e658a6b10516ad8b57a0
“Oreolv” <455001000@qq.com>
2022-04-27 21:33:43 +0800

    feat: 日志内容中增加用户信息

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(async (ctx, next) => {
   const start = new Date();
   await next();
   const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms ${JSON.stringify(ctx.user)}`);
 });
 
 // routes
