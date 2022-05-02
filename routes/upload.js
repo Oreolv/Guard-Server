f994df30b4e2cd6f9d278cada203c4787c1f6398
“Oreolv” <455001000@qq.com>
2022-05-02 15:34:54 +0800

    feat: 更改upload接口地址

--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')();
 const { uploadFile } = require('../controller/upload');
 
-router.post('/upload', async function (ctx, next) {
+router.post('/api/upload', async function (ctx, next) {
   const file = ctx.request.files.file; // 获取上传文件
   const result = await uploadFile(file);
   ctx.body = result;
