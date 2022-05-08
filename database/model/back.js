44c9ff31f6b8f3591b637485dc1cb348899b9390
“Oreolv” <455001000@qq.com>
2022-05-08 11:16:20 +0800

    feat: back表字段完善

--- database/model/back.js
+++ database/model/back.js
@@ -6,18 +6,54 @@ const Resident = require('./resident');
 const Back = sequelize.define(
   'back',
   {
-    status: Sequelize.INTEGER, // 审批状态
-    applicant: Sequelize.INTEGER, // 申请人
-    approver: Sequelize.INTEGER,
-    approve_time: Sequelize.DATE,
-    description: Sequelize.STRING,
-    end_time: Sequelize.DATE,
-    health_code: Sequelize.STRING, // 健康码与行程码截图
-    come_from: Sequelize.STRING,
-    vehicle: Sequelize.INTEGER(1), // 乘坐交通工具 0驾车1大巴2火车3高铁4飞机
-    vehicle_no: Sequelize.STRING(20), // 乘坐车牌号/车次/航班号
-    vehicle_seat: Sequelize.STRING(20), // 乘坐座位号，自驾则填无
-    risk_status: Sequelize.INTEGER(1), // 始发地风险等级
+    come_from: {
+      type: Sequelize.STRING(),
+      comment: '到达时间',
+    },
+    end_time: {
+      type: Sequelize.DATE,
+      comment: '到达时间',
+    },
+    health_code: {
+      type: Sequelize.STRING(200),
+      comment: '健康码与行程码截图',
+    },
+    risk_status: {
+      type: Sequelize.INTEGER(1),
+      comment: '始发地风险等级，0低1中2高风险',
+    },
+    vehicle: {
+      type: Sequelize.INTEGER(1),
+      comment: '乘坐交通工具 0驾车1大巴2火车3高铁4飞机',
+    },
+    vehicle_no: {
+      type: Sequelize.STRING(10),
+      comment: '乘坐车牌号/车次/航班号',
+    },
+    vehicle_seat: {
+      type: Sequelize.STRING(10),
+      comment: '乘坐座位号，自驾则填无',
+    },
+    status: {
+      type: Sequelize.INTEGER(1),
+      comment: '审批状态',
+    },
+    applicant: {
+      type: Sequelize.INTEGER,
+      comment: '申请人',
+    },
+    approver: {
+      type: Sequelize.INTEGER,
+      comment: '审批人',
+    },
+    approve_time: {
+      type: Sequelize.DATE,
+      comment: '审批时间',
+    },
+    description: {
+      type: Sequelize.STRING(50),
+      comment: '审批意见',
+    },
   },
   {
     paranoid: true,
