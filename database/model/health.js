6a4bdbbcd12c30849ffea87a9f14e77d62e1ceec
“Oreolv” <455001000@qq.com>
2022-05-08 11:36:43 +0800

    feat: health表字段完善

--- database/model/health.js
+++ database/model/health.js
@@ -6,16 +6,46 @@ const Resident = require('./resident');
 const Health = sequelize.define(
   'health',
   {
-    status: Sequelize.INTEGER, // 审批状态
-    applicant: Sequelize.INTEGER, // 申请人
-    approver: Sequelize.INTEGER,
-    approve_time: Sequelize.DATE,
-    description: Sequelize.STRING,
-    temperature: Sequelize.STRING,
-    diagnosis: Sequelize.INTEGER, // 是否为疑似、确诊患者
-    contact: Sequelize.INTEGER, // 是否接触疑似、确诊患者
-    symptom: Sequelize.INTEGER, // 是否有咳嗽、乏力等症状
-    hospital: Sequelize.STRING, // 就诊、住院、或无
+    temperature: {
+      type: Sequelize.STRING(15),
+      comment: '今日体温',
+    },
+    diagnosis: {
+      type: Sequelize.INTEGER(1),
+      comment: '是否为疑似、确诊患者，0否1是',
+    },
+    contact: {
+      type: Sequelize.INTEGER(1),
+      comment: '是否接触疑似、确诊患者，0否1是',
+    },
+    symptom: {
+      type: Sequelize.INTEGER(1),
+      comment: '是否有咳嗽、乏力等症状，0否1是',
+    },
+    hospital: {
+      type: Sequelize.INTEGER(1),
+      comment: '是否就诊或住院，无0、就诊1、住院2',
+    }, // 就诊、住院、或无
+    status: {
+      type: Sequelize.INTEGER(1),
+      comment: '审批状态',
+    },
+    applicant: {
+      type: Sequelize.INTEGER,
+      comment: '申请人',
+    },
+    approver: {
+      type: Sequelize.INTEGER,
+      comment: '审批人',
+    },
+    approve_time: {
+      type: Sequelize.DATE,
+      comment: '审批时间',
+    },
+    description: {
+      type: Sequelize.STRING(50),
+      comment: '审批意见',
+    },
   },
   {
     paranoid: true,
@@ -41,7 +71,7 @@ Resident.hasMany(Health, {
 });
 
 // (async () => {
-//   await Health.sync({ alter: true });
+//   await Health.sync({ force: true });
 // })();
 
 module.exports = Health;
