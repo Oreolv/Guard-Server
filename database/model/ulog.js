3f9460f9dbd02f8781aa1ab806abd180bb4d7c0d
“Oreolv” <455001000@qq.com>
2022-05-08 14:33:34 +0800

    feat: ulog表字段完善

--- database/model/ulog.js
+++ database/model/ulog.js
@@ -8,11 +8,24 @@ const Ulog = sequelize.define(
     userId: {
       type: Sequelize.INTEGER,
       allowNull: false,
+      comment: '用户ID',
     },
-    method: Sequelize.STRING,
-    group: Sequelize.STRING,
-    members: Sequelize.STRING,
-    params: Sequelize.TEXT,
+    method: {
+      type: Sequelize.STRING(8),
+      comment: '请求方法',
+    },
+    group: {
+      type: Sequelize.STRING(15),
+      comment: '请求对象',
+    },
+    members: {
+      type: Sequelize.STRING(15),
+      comment: '请求函数',
+    },
+    params: {
+      type: Sequelize.TEXT('tiny'),
+      comment: '请求参数',
+    },
   },
   {
     paranoid: true,
